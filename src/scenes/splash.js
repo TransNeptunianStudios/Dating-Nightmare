0120cb466658b2e8ea06990982b45c503745092e
Mikael Larsson <c.mikael.larsson@gmail.com>
2018-01-06 00:17:22 +0100

    Modified splash scene.

--- src/scenes/splash.js
+++ src/scenes/splash.js
@@ -9,32 +9,121 @@ export default class Splash extends Phaser.Scene {
   }
 
   preload () {
-    this.load.image('logo', 'assets/images/TNSLogo.png')
+    this.load.atlas(
+      'tns_logo',
+      'assets/images/tns_logo_ss.png',
+      'assets/images/tns_logo_ss.json')
   }
 
   create () {
-    var logo = this.add.image(400, 200, 'logo')
-    logo.alpha = 0
+    var centerX = this.game.config.width / 2
+    var centerY = this.game.config.height / 2
 
+    this.logo = this.add.image(
+      centerX,
+      centerY,
+      'tns_logo',
+      'nept_logo_stars').setAlpha(0.0);
+
+    this.star1 = this.add.image(
+      centerX + 67,
+      centerY - 91,
+      'tns_logo',
+      'star1').setAlpha(0.0)
+
+    this.star2 = this.add.image(
+      centerX - 73,
+      centerY + 83,
+      'tns_logo',
+      'star2').setAlpha(0.0)
+
+    this.star3 = this.add.image(
+      centerX - 77,
+      centerY - 87,
+      'tns_logo',
+      'star3').setAlpha(0.0)
+
+    this.star4 = this.add.image(
+      centerX + 87,
+      centerY + 33,
+      'tns_logo',
+      'star4').setAlpha(0.0)
+
+    this.star5 = this.add.image(
+      centerX - 84,
+      centerY + 7,
+      'tns_logo',
+      'star1').setAlpha(0.0)
+
+    var fadeInTimeline = this.tweens.createTimeline()
+
+    this.addFadeInStar(fadeInTimeline, this.star1, 2000)
+    this.addFadeInStar(fadeInTimeline, this.star2, 1000)
+    this.addFadeInStar(fadeInTimeline, this.star3, 1000)
+    this.addFadeInStar(fadeInTimeline, this.star4, 1000)
+    this.addFadeInStar(fadeInTimeline, this.star5, 1000)
+    this.addFadeInLogo(fadeInTimeline)
+
+    this.input.events.on(
+      'POINTER_DOWN_EVENT',
+      this.endScene,
+      0,
+      this)
+
+    fadeInTimeline.play()
+  }
+
+  addFadeInStar (timeline, star, duration) {
+    timeline.add({
+      targets: star,
+      alpha: 1.0,
+      ease: 'Power2',
+      duration: duration
+    })
+  }
+
+  addFadeOutStar (star) {
     this.tweens.add({
-      targets: logo,
-      alpha: 1,
+      targets: star,
+      alpha: 0.0,
+      ease: 'Power2',
+      duration: 3000
+    })
+  }
+
+  addFadeInLogo (timeline) {
+    timeline.add({
+      targets: this.logo,
+      alpha: 1.0,
       duration: 3000,
       ease: 'Power2',
-      yoyo: true,
-      onComplete: this.endScene,
+      onComplete: this.fadeOut,
       callbackScope: this,
       completeDelay: 2000
     })
-
-    this.input.events.on('POINTER_DOWN_EVENT',
-                       this.endScene,
-                       0,
-                       this)
-
-    console.log('Splash created')
   }
+
+  addFadeOutLogo () {
+    this.tweens.add({
+      targets: this.logo,
+      alpha: 0.0,
+      duration: 3000,
+      ease: 'Power2',
+      onComplete: this.endScene,
+      callbackScope: this
+    })
+  }
+
+  fadeOut () {
+    this.addFadeOutStar(this.star1)
+    this.addFadeOutStar(this.star2)
+    this.addFadeOutStar(this.star3)
+    this.addFadeOutStar(this.star4)
+    this.addFadeOutStar(this.star5)
+    this.addFadeOutLogo()
+  }
+
   endScene () {
     this.scene.start('Skyline')
   }
-};
+}
